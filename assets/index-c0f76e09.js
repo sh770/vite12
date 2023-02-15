3e6d9426f81d3121e326b898333e7ba1e87f7ed7
sh770 <sh770@users.noreply.github.com>
2023-02-15 20:46:04 +0000

    deploy: 394dea28aa138b8f8129c90c8aecde79bfb06a48

---
+++ assets/index-c0f76e09.js
@@ -0,0 +1,17 @@
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const c="/vite12/assets/javascript-8dac5379.svg",l="/vite12/assets/vite-4a748afd.svg";function a(n){let o=0;const r=s=>{o=s,n.innerHTML=`count is ${o}`};n.addEventListener("click",()=>r(o+1)),r(0)}document.querySelector("#app").innerHTML=`
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${l}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${c}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>שלום Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      לחץ על הלוגו של  Vite כדי ללמוד עוד
+    </p>
+  </div>
+`;a(document.querySelector("#counter"));

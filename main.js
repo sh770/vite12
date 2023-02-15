394dea28aa138b8f8129c90c8aecde79bfb06a48
sh <sh770h@gmail.com>
2023-02-15 22:45:08 +0200

    he 12

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>שלום Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      לחץ על הלוגו של  Vite כדי ללמוד עוד
     </p>
   </div>
 `
